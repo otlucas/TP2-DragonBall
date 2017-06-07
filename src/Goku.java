1ae53ad66a2834c9f2415860c688a8acb09644b8
otlucas <otero.lucas@outlook.com>
2017-06-06 22:37:17 -0300

    Subclases de Personaje

---
+++ src/Goku.java
@@ -0,0 +1,48 @@
+
+public class Goku extends Personaje {
+	
+	public Goku(){
+		this.nombre = "Goku";
+		this.modo = "Normal";
+		this.puntosDeVidaMaximos = 500;
+		this.puntosDeVida = 500;
+		this.poderDePelea = 20;
+		this.distanciaDeAtaque = 2;
+		this.velocidadDeDesplazamiento = 2;
+		this.ki = 0;
+	}
+	
+	public int ejecutarAtaqueEspecial(int daño){
+		daño = daño + (50/100)*daño;
+		ki = ki-20;
+		return daño;
+	}
+	
+	public boolean puedeEfectuarAtaqueEspecial(){
+		return (ki >= 20);
+	}
+	
+	public boolean puedeEfectuarPrimeraTransformacion(){
+		return ((ki >= 20) && (modo == "Normal"));
+	}
+	
+	public boolean puedeEfectuarSegundaTransformacion(){
+		return ((ki >= 50) && (modo == "Kaio-Ken"));
+	}
+	
+	public void efectuarPrimeraTransformacion(){
+		modo = "Kaio-Ken";
+		poderDePelea = 40;
+		distanciaDeAtaque = 4;
+		velocidadDeDesplazamiento = 3;
+		ki = ki-20;
+	}
+	
+	public void efectuarSegundaTransformacion(){
+		modo = "Super Sayajin";
+		poderDePelea = 60;
+		velocidadDeDesplazamiento = 5;
+		ki = ki-50;
+	}
+
+}

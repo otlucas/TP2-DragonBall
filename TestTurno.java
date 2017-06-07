819f6d87ccf934cf647d06e61ed830383378b008
Germán Rotili <German-Rotili@users.noreply.github.com>
2017-06-07 00:10:05 -0300

    Add files via upload

---
+++ TestTurno.java
@@ -0,0 +1,112 @@
+package contenido;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTurno {
+
+	protected Turno testTurno = new Turno();
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void testDevolverEquipo() {
+		
+		int equipo = testTurno.devolverEquipo();
+		
+		Assert.assertEquals(0, equipo);
+	}
+	
+	public void testDevolverEquipoInicial() {
+		
+		testTurno.turnoInicial();
+		
+		int equipo = testTurno.devolverEquipo();
+		
+		int equipoInicial = testTurno.devolverEquipoInicial();
+		
+		Assert.assertTrue(equipo == equipoInicial);
+	}
+	
+	public void testDevolverNumeroDeTurno() {
+		
+		testTurno.turnoInicial();
+		
+		int numeroDeTurno = testTurno.devolverNumeroDeTurno();
+		
+		Assert.assertTrue(1 == numeroDeTurno);
+	}
+	
+	public void testFinalizarTurno() {
+		
+		testTurno.turnoInicial();
+		
+		int equipo = testTurno.devolverEquipo();
+		
+		int equipoInicial = testTurno.devolverEquipoInicial();
+		
+		Assert.assertTrue(equipo == equipoInicial);
+		
+		testTurno.finalizarTurno();
+		
+		equipo = testTurno.devolverEquipo();
+		
+		Assert.assertFalse(equipo != equipoInicial);
+	}
+	
+	public void testCambioDeEquipoSinCambioDeTurno() {
+		
+		testTurno.turnoInicial();
+		
+		int numeroDeTurno = testTurno.devolverNumeroDeTurno();
+		
+		int equipo = testTurno.devolverEquipo();
+		
+		int equipoInicial = testTurno.devolverEquipoInicial();
+		
+		Assert.assertTrue(((equipo == equipoInicial) && (1 == numeroDeTurno)));
+		
+		testTurno.finalizarTurno();
+		
+		numeroDeTurno = testTurno.devolverNumeroDeTurno();
+		
+		equipo = testTurno.devolverEquipo();
+		
+		Assert.assertTrue(((equipo != equipoInicial) && (1 == numeroDeTurno)));
+	}
+	
+	public void testFinalizarTurnoVarios() {
+		
+		testTurno.turnoInicial();
+		
+		int equipo = testTurno.devolverEquipo();
+		
+		int equipoInicial = testTurno.devolverEquipoInicial();
+		
+		Assert.assertTrue(equipo == equipoInicial);
+		
+		testTurno.finalizarTurno();
+		
+		equipo = testTurno.devolverEquipo();
+		
+		Assert.assertFalse(equipo != equipoInicial);
+		
+		int numeroDeTurno = testTurno.devolverNumeroDeTurno();
+		
+		Assert.assertTrue(1 == numeroDeTurno);
+		
+		testTurno.finalizarTurno();
+		
+		equipo = testTurno.devolverEquipo();
+		
+		Assert.assertFalse(equipo == equipoInicial);
+		
+		numeroDeTurno = testTurno.devolverNumeroDeTurno();
+		
+		Assert.assertTrue(2 == numeroDeTurno);
+	}
+}
---
+++ Turno.java
@@ -0,0 +1,47 @@
+package contenido;
+
+import java.util.Random;
+
+public class Turno {
+	
+	private int numeroDeTurno;
+	
+	private int equipo;
+	
+	private int equipoInicial;
+	
+	public void turnoInicial() {
+		 equipo = 1;
+		 numeroDeTurno = 1;
+		 Random randomGenerator = new Random();
+		 int randomInt = randomGenerator.nextInt(100);
+		 if (randomInt % 2 == 0) {
+			 equipo = 2;
+		 }
+		 equipoInicial = equipo;
+	}
+	
+	public void finalizarTurno() {
+		if (equipo == 1) {
+			equipo = 2;
+		}
+		else {
+			equipo = 1;
+		}
+		if (equipo == equipoInicial) {
+			numeroDeTurno ++;
+		}
+	}
+	
+	public int devolverEquipo() {
+		return equipo;
+	}
+	
+	public int devolverNumeroDeTurno() {
+		return numeroDeTurno;
+	}
+	
+	public int devolverEquipoInicial() {
+		return equipoInicial;
+	}
+}

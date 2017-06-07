533a5941e865efd5f332b84e31745eca59c6b38f
FMAA1997 <fede.alvarez1997@gmail.com>
2017-06-07 21:49:07 +0200

    Add files via upload

---
+++ src/FreezerTests.java
@@ -0,0 +1,191 @@
+package tp2Tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import tp2.CondicionesInsuficientes;
+import tp2.Freezer;
+
+public class FreezerTests {
+	
+	@Test
+	public void test01ObtenerPoderDePelea(){
+		Freezer frio = new Freezer();
+		
+		assertTrue(frio.getPoderDePelea() == 20);
+	}
+	
+	@Test
+	public void test02ObtenerPuntosDeVida(){
+		Freezer freezer = new Freezer();
+		
+		assertTrue(freezer.obtenerPuntosDeVida() == 400);
+	}
+	
+	@Test
+	public void test03ObtenerDistanciaDeAtaque(){
+		Freezer freezer = new Freezer();
+		
+		assertTrue(freezer.getdistanciaDeAtaque() == 2);
+	}
+	
+	@Test
+	public void test04ObtenerVelocidadDeDesplazamiento(){
+		Freezer freezer = new Freezer();
+		
+		assertTrue(freezer.getVelocidadDeDesplazamiento() == 4);
+	}
+	
+	@Test
+	public void test05PerderPuntosDeVida(){
+		Freezer freezer = new Freezer();
+		freezer.perderPuntosDeVida(140);
+		
+		assertTrue(freezer.obtenerPuntosDeVida() == 260);
+	}
+	
+	@Test
+	public void test06GanarPuntosDeVida(){
+		Freezer freezer = new Freezer();
+		freezer.perderPuntosDeVida(140);
+		freezer.ganarPuntosDeVida(50);
+		
+		assertTrue(freezer.obtenerPuntosDeVida() == 310);		
+	}
+	
+	@Test
+	public void test07PuedeEfectuarPrimeraTransformacionInicialmenteDaFalse(){
+		Freezer freezer = new Freezer();
+		
+		assertFalse(freezer.puedeEfectuarPrimeraTransformacion());
+	}
+	
+	@Test
+	public void test08PuedeEfectuarAtaqueEspecialInicialmenteDaFalse(){
+		Freezer freezer = new Freezer();
+		
+		assertFalse(freezer.puedeEfectuarAtaqueEspecial());
+	}
+	
+	@Test
+	public void test09PuedeEfectuarSegundaTransformacionInicialmenteDaFalse(){
+		Freezer freezer = new Freezer();
+		
+		assertFalse(freezer.puedeEfectuarSegundaTransformacion());
+	}
+	
+	@Test
+	public void test10LuegoDeVariosTurnosEfectuarPrimeraTrasformacionEsTrue(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<5; i++)
+			freezer.ganarKi();
+		
+		assertTrue(freezer.puedeEfectuarPrimeraTransformacion());
+	}
+	
+	@Test
+	public void test11SinEfectuarPrimeraTransformacionNoPuedeEfectuarSegunda(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<10; i++)
+			freezer.ganarKi();
+		
+		assertFalse(freezer.puedeEfectuarSegundaTransformacion());
+	}
+	
+	@Test
+	public void test12LuegoDeVariosTurnosPuedeEfectuarAtaqueEspecialEsTrue(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<8; i++)
+			freezer.ganarKi();
+		
+		assertTrue(freezer.puedeEfectuarAtaqueEspecial());
+	}
+	
+	@Test
+	public void test13LuegoDeEfectuarPrimeraPuedeEfectuarSegundaTransformacionEsTrue(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<20; i++)
+			freezer.ganarKi();
+		freezer.efectuarPrimeraTransformacion();
+		
+		assertTrue(freezer.puedeEfectuarSegundaTransformacion());
+	}
+	
+	@Test
+	public void test14EfectuarPrimeraTransformacionCambioPoderDePelea(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<8; i++)
+			freezer.ganarKi();
+		freezer.efectuarPrimeraTransformacion();
+		
+		assertTrue(freezer.getPoderDePelea() == 40);
+	}
+	
+	@Test
+	public void test15EfectuarPrimeraTransformacionCambioDistanciaDeAtaque(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<8; i++)
+			freezer.ganarKi();
+		freezer.efectuarPrimeraTransformacion();
+		
+		assertTrue(freezer.getdistanciaDeAtaque() == 3);
+	}
+	
+	@Test
+	public void test16EfectuarSegundaTransformacionCambioPoderDePelea(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<25; i++)
+			freezer.ganarKi();
+		freezer.efectuarPrimeraTransformacion();
+		freezer.efectuarSegundaTransformacion();
+		
+		assertTrue(freezer.getPoderDePelea() == 50);
+	}
+	
+	@Test
+	public void test17EfectuarSegundaTransformacionCambioVelocidadDeDesplazamiento(){
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<25; i++)
+			freezer.ganarKi();
+		freezer.efectuarPrimeraTransformacion();
+		freezer.efectuarSegundaTransformacion();
+		
+		assertTrue(freezer.getVelocidadDeDesplazamiento() == 6);
+	}
+	
+	@Test (expected = CondicionesInsuficientes.class)
+	public void test18UsarPrimeraTransformacionInicialmenteLanzaCondicionesInsuficientes() throws CondicionesInsuficientes{
+		Freezer freezer = new Freezer();
+		freezer.usarPrimeraTransformacion();
+	}
+	
+	@Test (expected = CondicionesInsuficientes.class)
+	public void test19UsarSegundaTransformacionInicialmenteLanzaCondicionesInsuficientes() throws CondicionesInsuficientes{
+		Freezer freezer = new Freezer();
+		freezer.usarSegundaTransformacion();
+	}
+	
+	@Test 
+	public void test20SePuedeUsarPrimeraTransformacionLuegoDeTurnos() throws CondicionesInsuficientes{
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<25; i++)
+			freezer.ganarKi();
+		freezer.usarPrimeraTransformacion();
+		
+		assertTrue((freezer.getPoderDePelea() == 40) && (freezer.getdistanciaDeAtaque() == 3));
+	}
+	
+	@Test 
+	public void test21SePuedeUsarSegundaTransformacionLuegoDeTurnos() throws CondicionesInsuficientes{
+		Freezer freezer = new Freezer();
+		for (int i = 0; i<25; i++)
+			freezer.ganarKi();
+		freezer.usarPrimeraTransformacion();
+		freezer.usarSegundaTransformacion();
+		
+		assertTrue((freezer.getPoderDePelea() == 50) && (freezer.getVelocidadDeDesplazamiento() == 6) && (freezer.getdistanciaDeAtaque() == 3));
+	}
+
+}
---
+++ src/MajinBooTests.java
@@ -0,0 +1,204 @@
+package tp2Tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import tp2.CondicionesInsuficientes;
+import tp2.MajinBoo;
+
+public class MajinBooTests {
+	
+	@Test
+	public void test01ObtenerPoderDePelea(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertTrue(majin.getPoderDePelea() == 30);
+	}
+	
+	@Test
+	public void test02ObtenerPuntosDeVida(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertTrue(majin.obtenerPuntosDeVida() == 300);
+	}
+	
+	@Test
+	public void test03ObtenerDistanciaDeAtaque(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertTrue(majin.getdistanciaDeAtaque() == 2);
+	}
+	
+	@Test
+	public void test04ObtenerVelocidadDeDesplazamiento(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertTrue(majin.getVelocidadDeDesplazamiento() == 2);
+	}
+	
+	@Test
+	public void test05PerderPuntosDeVida(){
+		MajinBoo majin = new MajinBoo();
+		majin.perderPuntosDeVida(140);
+		
+		assertTrue(majin.obtenerPuntosDeVida() == 160);
+	}
+	
+	@Test
+	public void test06GanarPuntosDeVida(){
+		MajinBoo majin = new MajinBoo();
+		majin.perderPuntosDeVida(140);
+		majin.ganarPuntosDeVida(50);
+		
+		assertTrue(majin.obtenerPuntosDeVida() == 210);		
+	}
+	
+	@Test
+	public void test07PuedeEfectuarPrimeraTransformacionInicialmenteDaFalse(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertFalse(majin.puedeEfectuarPrimeraTransformacion());
+	}
+	
+	@Test
+	public void test08PuedeEfectuarAtaqueEspecialInicialmenteDaFalse(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertFalse(majin.puedeEfectuarAtaqueEspecial());
+	}
+	
+	@Test
+	public void test09PuedeEfectuarSegundaTransformacionInicialmenteDaFalse(){
+		MajinBoo majin = new MajinBoo();
+		
+		assertFalse(majin.puedeEfectuarSegundaTransformacion());
+	}
+	
+	@Test
+	public void test10LuegoDeVariosTurnosEfectuarPrimeraTrasformacionEsTrue(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<5; i++)
+			majin.ganarKi();
+		
+		assertTrue(majin.puedeEfectuarPrimeraTransformacion());
+	}
+	
+	@Test
+	public void test11SinEfectuarPrimeraTransformacionNoPuedeEfectuarSegunda(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<10; i++)
+			majin.ganarKi();
+		
+		assertFalse(majin.puedeEfectuarSegundaTransformacion());
+	}
+	
+	@Test
+	public void test12LuegoDeVariosTurnosPuedeEfectuarAtaqueEspecialEsTrue(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<8; i++)
+			majin.ganarKi();
+		
+		assertTrue(majin.puedeEfectuarAtaqueEspecial());
+	}
+	
+	@Test
+	public void test13LuegoDeEfectuarPrimeraPuedeEfectuarSegundaTransformacionEsTrue(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<20; i++)
+			majin.ganarKi();
+		majin.efectuarPrimeraTransformacion();
+		
+		assertTrue(majin.puedeEfectuarSegundaTransformacion());
+	}
+	
+	@Test
+	public void test14EfectuarPrimeraTransformacionCambioPoderDePelea(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<8; i++)
+			majin.ganarKi();
+		majin.efectuarPrimeraTransformacion();
+		
+		assertTrue(majin.getPoderDePelea() == 50);
+	}
+	
+	@Test
+	public void test15EfectuarPrimeraTransformacionCambioVelocidadDeDesplazamiento(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<8; i++)
+			majin.ganarKi();
+		majin.efectuarPrimeraTransformacion();
+		
+		assertTrue(majin.getVelocidadDeDesplazamiento() == 3);
+	}
+	
+	@Test
+	public void test16EfectuarSegundaTransformacionCambioPoderDePelea(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<25; i++)
+			majin.ganarKi();
+		majin.efectuarPrimeraTransformacion();
+		majin.efectuarSegundaTransformacion();
+		
+		assertTrue(majin.getPoderDePelea() == 60);
+	}
+	
+	@Test
+	public void test17EfectuarSegundaTransformacionCambioVelocidadDeDesplazamiento(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<25; i++)
+			majin.ganarKi();
+		majin.efectuarPrimeraTransformacion();
+		majin.efectuarSegundaTransformacion();
+		
+		assertTrue(majin.getVelocidadDeDesplazamiento() == 4);
+	}
+	
+	@Test
+	public void test18EfectuarSegundaTransformacionCambioDistanciaDeAtaque(){
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<25; i++)
+			majin.ganarKi();
+		majin.efectuarPrimeraTransformacion();
+		majin.efectuarSegundaTransformacion();
+		
+		assertTrue(majin.getdistanciaDeAtaque() == 3);
+	}
+	
+	@Test (expected = CondicionesInsuficientes.class)
+	public void test19UsarPrimeraTransformacionInicialmenteLanzaCondicionesInsuficientes() throws CondicionesInsuficientes{
+		MajinBoo majin = new MajinBoo();
+		majin.usarPrimeraTransformacion();
+	}
+	
+	@Test (expected = CondicionesInsuficientes.class)
+	public void test20UsarSegundaTransformacionInicialmenteLanzaCondicionesInsuficientes() throws CondicionesInsuficientes{
+		MajinBoo majin = new MajinBoo();
+		majin.usarSegundaTransformacion();
+	}
+	
+	@Test 
+	public void test21SePuedeUsarPrimeraTransformacionLuegoDeTurnos() throws CondicionesInsuficientes{
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<25; i++)
+			majin.ganarKi();
+		majin.usarPrimeraTransformacion();
+		
+		assertTrue((majin.getPoderDePelea() == 50) && (majin.getVelocidadDeDesplazamiento() == 3));
+	}
+	
+	@Test 
+	public void test22SePuedeUsarSegundaTransformacionLuegoDeTurnos() throws CondicionesInsuficientes{
+		MajinBoo majin = new MajinBoo();
+		for (int i = 0; i<25; i++)
+			majin.ganarKi();
+		majin.usarPrimeraTransformacion();
+		majin.usarSegundaTransformacion();
+		
+		assertTrue((majin.getPoderDePelea() == 60) && (majin.getVelocidadDeDesplazamiento() == 4) && (majin.getdistanciaDeAtaque() == 3));
+	}
+
+
+
+}

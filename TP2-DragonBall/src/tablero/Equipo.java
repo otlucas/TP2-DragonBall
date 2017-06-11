a84b52f77275da0a39788ec856ec82e1983f12ff
SantiAlme <santi.alme@gmail.com>
2017-06-11 16:54:57 -0300

    Equipo
    Hice lo básico que se me ocurrió que tenía que tener un equipo. Si le falta o sobra algo, cambienlo.

--- TP2-DragonBall/src/tablero/Equipo.java
+++ TP2-DragonBall/src/tablero/Equipo.java
@@ -1,5 +1,33 @@
 package tablero;
 
-public class Equipo {
+import personaje.Personaje;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Equipo {
+	
+	private String nombre;
+	private int cantidadDeEsferasDelDragon = 0;
+	private List<Personaje> integrantes;
+	
+	public Equipo(String nombre){
+		this.nombre = nombre;
+		this.integrantes = new ArrayList<Personaje>();
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public void agregarPersonaje(Personaje personaje){
+		integrantes.add(personaje);
+	}
+	
+	public void incrementarEsferasDelDragon(){
+		cantidadDeEsferasDelDragon++;
+	}
+	
+	public int contarEsferasDelDragon(){
+		return cantidadDeEsferasDelDragon;
+	}
 }

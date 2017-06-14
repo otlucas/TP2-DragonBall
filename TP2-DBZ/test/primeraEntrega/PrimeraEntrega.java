e3d0ab9359a75a83681045f26508c68088c859cc
otlucas <otero.lucas@outlook.com>
2017-06-15 00:27:17 +0200

    Actualizado para las ultimas modificaciones

--- TP2-DBZ/test/primeraEntrega/PrimeraEntrega.java
+++ TP2-DBZ/test/primeraEntrega/PrimeraEntrega.java
@@ -1,17 +1,19 @@
-package primeraEntrega;
-/*package personajetests;*/
+
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
 import org.junit.Test;
 
+import sistema.AtaqueNoValido;
+import sistema.Sistema;
 import personaje.Cell;
 import personaje.Gohan;
 import personaje.Goku;
 import personaje.Personaje;
-import tablero.AtaqueNoValido;
 import tablero.CasilleroOcupado;
 import tablero.MovimientoNoValido;
 import tablero.NombreDePersonajeNoValido;
@@ -23,89 +25,51 @@ public class PrimeraEntrega {
     public void testUbicarMoverYVerificarNuevaPosicion() throws MovimientoNoValido, CasilleroOcupado, NombreDePersonajeNoValido {
 
         
-        ArrayList<Personaje> personajes = new ArrayList<Personaje>();
         Goku testGoku = new Goku();
         
-        personajes.add(testGoku);
+        Tablero tablero = new Tablero(5);
         
-        Tablero tablero = new Tablero(5, personajes, null, null);
-        
-        testGoku.casilleroActual = tablero.tablero.get(0).get(0);
-        
-        tablero.tablero.get(0).get(0).cambiarEstado();
-        
-        tablero.personajeSeMueveHasta("Goku", tablero.tablero.get(2).get(1));
-        
-        assertEquals(tablero.tablero.get(2).get(1), testGoku.casilleroActual);
+        assertTrue(tablero.posicionar(testGoku, tablero.getTablero()[0][0]));
 		
 	}
 	
-	@Test(expected=CasilleroOcupado.class)
+	@Test
     public void testDosPersonajesEnUnCasillero() throws MovimientoNoValido, CasilleroOcupado, NombreDePersonajeNoValido {
 
         
-        ArrayList<Personaje> personajes = new ArrayList<Personaje>();
         Goku testGoku = new Goku();
         Cell testCell = new Cell();
         
-        personajes.add(testGoku);
-        personajes.add(testCell);
+        Tablero tablero = new Tablero(5);
         
-        Tablero tablero = new Tablero(5, personajes, null, null);
-        
-        testGoku.casilleroActual = tablero.tablero.get(0).get(0);
-        testCell.casilleroActual = tablero.tablero.get(0).get(1);
-        
-        tablero.tablero.get(0).get(0).cambiarEstado();
-        tablero.tablero.get(0).get(1).cambiarEstado();
-        
-        tablero.personajeSeMueveHasta("Goku", tablero.tablero.get(2).get(1));
-        assertEquals(tablero.tablero.get(2).get(1), testGoku.casilleroActual);
-        
-        //Luego esto tira excepcion
-        tablero.personajeSeMueveHasta("Cell", tablero.tablero.get(2).get(1));
+        tablero.posicionar(testGoku, tablero.getTablero()[0][0]);
+        assertFalse(tablero.posicionar(testCell, tablero.getTablero()[0][0]));
 	}
 	
-	@Test(expected=MovimientoNoValido.class)
+	@Test(expected=CasilleroOcupado.class)
     public void testUnPersonajeNoPasaPorEncimaDeOtro() throws MovimientoNoValido, CasilleroOcupado, NombreDePersonajeNoValido {
-		//Para probarlo mejor agrego tres personajes, porque los costos para ir en diagonal son iguales a los de ir en linea
-		//recta
         
-        ArrayList<Personaje> personajes = new ArrayList<Personaje>();
         Goku testGoku = new Goku();
         Cell testCell = new Cell();
-        Gohan testGohan = new Gohan();
         
-        personajes.add(testGoku);
-        personajes.add(testCell);
-        personajes.add(testGohan);
+        Tablero tablero = new Tablero(5);
         
-        Tablero tablero = new Tablero(5, personajes, null, null);
+        tablero.posicionar(testGoku, tablero.getTablero()[0][0]);
+        tablero.posicionar(testCell, tablero.getTablero()[0][1]);
         
-        testGoku.casilleroActual = tablero.tablero.get(0).get(0);
-        testCell.casilleroActual = tablero.tablero.get(0).get(1);
-        testGohan.casilleroActual = tablero.tablero.get(1).get(1);
         
-        tablero.tablero.get(0).get(0).cambiarEstado();
-        tablero.tablero.get(0).get(1).cambiarEstado();
-        tablero.tablero.get(1).get(1).cambiarEstado();
-        //Si pudiese pasar por encima entonces esto no lanzaria una excepcion (los tiene que rodear)
-        tablero.personajeSeMueveHasta("Goku", tablero.tablero.get(0).get(2));
+        //Si pudiese pasar por encima entonces esto no lanzaria una excepcion
+        tablero.mover(tablero.getTablero()[0][0], tablero.getTablero()[0][1]);
 	}
 	
 	@Test
     public void testPersonajeSeTransforma() throws MovimientoNoValido, CasilleroOcupado, NombreDePersonajeNoValido {
         
-        ArrayList<Personaje> personajes = new ArrayList<Personaje>();
         Goku testGoku = new Goku();
         
-        personajes.add(testGoku);
+        Tablero tablero = new Tablero(5);
         
-        Tablero tablero = new Tablero(5, personajes, null, null);
-        
-        testGoku.casilleroActual = tablero.tablero.get(0).get(0);
-        
-        tablero.tablero.get(0).get(0).cambiarEstado();
+        tablero.posicionar(testGoku, tablero.getTablero()[0][0]);
         
         for (int i = 0; i<5; i++){
 			testGoku.ganarKi(); //Tengo que hacer que gane ki
@@ -121,63 +85,60 @@ public class PrimeraEntrega {
 	@Test
     public void testPersonajeSeTransformaYSeMueve() throws MovimientoNoValido, CasilleroOcupado, NombreDePersonajeNoValido {
         
-        ArrayList<Personaje> personajes = new ArrayList<Personaje>();
         Goku testGoku = new Goku();
         
-        personajes.add(testGoku);
+        Tablero tablero = new Tablero(5);
         
-        Tablero tablero = new Tablero(5, personajes, null, null);
+        tablero.posicionar(testGoku, tablero.getTablero()[0][0]);
         
-        testGoku.casilleroActual = tablero.tablero.get(0).get(0);
-        
-        tablero.tablero.get(0).get(0).cambiarEstado();
-        
-        try{
-        	tablero.personajeSeMueveHasta("Goku", tablero.tablero.get(0).get(3));
-        }catch(MovimientoNoValido e){
-        	System.out.println("No se puede porque Goku tiene 2 de velocidad de Desplazamiento");
-        }
         for (int i = 0; i<5; i++){
 			testGoku.ganarKi(); //Tengo que hacer que gane ki
         }
         assertTrue(testGoku.puedeEfectuarPrimeraTransformacion());
         testGoku.efectuarPrimeraTransformacion();
-        tablero.personajeSeMueveHasta("Goku", tablero.tablero.get(0).get(3));
-        assertEquals(tablero.tablero.get(0).get(3), testGoku.casilleroActual);
+        tablero.mover(tablero.getTablero()[0][0], tablero.getTablero()[0][1]);
+        assertTrue(tablero.getTablero()[0][1].estaOcupado());
+        
 	}
 	
 	@Test
-    public void testDosPersonajesSeAtacan() throws NombreDePersonajeNoValido, AtaqueNoValido {
-		//Para probarlo mejor agrego tres personajes, porque los costos para ir en diagonal son iguales a los de ir en linea
-		//recta
-        
+    public void testDosPersonajesSeAtacan() throws Exception {
+
         ArrayList<Personaje> personajes = new ArrayList<Personaje>();
         Goku testGoku = new Goku();
         Cell testCell = new Cell();
         
-        personajes.add(testGoku);
         personajes.add(testCell);
+        personajes.add(testGoku);
+                
+        Tablero tablero = new Tablero(8);
         
-        Tablero tablero = new Tablero(5, personajes, null, null);
+        Sistema sistema = new Sistema(personajes, null, null, tablero);
         
-        testGoku.casilleroActual = tablero.tablero.get(0).get(0);
-        testCell.casilleroActual = tablero.tablero.get(0).get(1);
+        tablero.posicionar(testGoku, tablero.getTablero()[0][0]);
+        tablero.posicionar(testCell, tablero.getTablero()[0][1]);
 
-        tablero.personajeUsaAtaqueBasicoContra("Goku", "Cell"); //Dentro del rango
+        sistema.atacar(tablero.getTablero()[0][0], tablero.getTablero()[0][1], false); //Dentro del rango
         assertEquals(testCell.obtenerPuntosDeVida(), 480);
-        tablero.personajeUsaAtaqueBasicoContra("Cell", "Goku"); //Dentro del rango
+        sistema.atacar(tablero.getTablero()[0][1], tablero.getTablero()[0][0], false); //Dentro del rango
         assertEquals(testGoku.obtenerPuntosDeVida(), 480);
         
-        testGoku.casilleroActual = tablero.tablero.get(4).get(4); // Alejo un personje, fuera del rango de los dos
+        tablero.mover(tablero.getTablero()[0][0], tablero.getTablero()[1][1]); // Alejo un personaje fuera de su rango
+        tablero.mover(tablero.getTablero()[1][1], tablero.getTablero()[1][2]);
+        tablero.mover(tablero.getTablero()[1][2], tablero.getTablero()[1][3]);
+        tablero.mover(tablero.getTablero()[1][3], tablero.getTablero()[1][4]);
+        tablero.mover(tablero.getTablero()[1][4], tablero.getTablero()[1][5]);
+        tablero.mover(tablero.getTablero()[1][5], tablero.getTablero()[1][6]);
+        tablero.mover(tablero.getTablero()[1][6], tablero.getTablero()[1][7]);
         
         try{
-        	tablero.personajeUsaAtaqueBasicoContra("Goku", "Cell"); //Fuera del rango
+        	 sistema.atacar(tablero.getTablero()[1][7], tablero.getTablero()[0][1], false); //Fuera del rango
         }catch(AtaqueNoValido e){
         }
         assertEquals(testCell.obtenerPuntosDeVida(), 480); //Siguen igual, no se efectuó el ataque
         
         try{
-        	tablero.personajeUsaAtaqueBasicoContra("Cell", "Goku"); //Fuera del rango
+        	sistema.atacar(tablero.getTablero()[0][1], tablero.getTablero()[1][7], false); //Fuera del rango
         }catch(AtaqueNoValido e){
         }
         assertEquals(testGoku.obtenerPuntosDeVida(), 480); //Siguen igual, no se efectuó el ataque

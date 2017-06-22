6225cb68db8cc15dab24e66f4e3a0058acdf6e34
otlucas <otero.lucas@outlook.com>
2017-06-22 02:03:56 +0200

    Delete Estado.java

--- TP2-DBZ/src/personaje/Estado.java
+++
@@ -1,9 +0,0 @@
-package personaje;
-
-public interface Estado {
-	
-	public abstract int getPoderDePelea(Personaje personaje);
-	public abstract int getDistanciaDeAtaque(Personaje personaje);
-	public abstract int getVelocidadDeDesplazamiento(Personaje personaje);
-	
-}
